096c73eb16d5f270a42311c6446bd0cdef9a58ae
fatima <fatimafouad9999@gmail.com>
2024-01-29 16:56:53 +0300

    links works and added some css

--- express-dev-skills/routes/index.js
+++ express-dev-skills/routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+var express = require("express")
+var router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" })
+})
 
-module.exports = router;
+router.get("/", function (req, res, next) {
+  res.render("show", { title: "Express" })
+})
+module.exports = router
